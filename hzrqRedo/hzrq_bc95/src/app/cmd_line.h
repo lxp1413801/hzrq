ec4b1d2c6203cbfee4becd12bb7021aa5ea6768f
lxp1413801 <lxp1413801@gmail.com>
2019-03-01 10:46:19 +0800

    first commit

---
+++ hzrqRedo/hzrq_bc95/src/app/cmd_line.h
@@ -0,0 +1,13 @@
+//file name :cmd_line.h
+#ifndef __cmd_line_h__
+#define __cmd_line_h__
+#ifdef __cplusplus
+	extern "C"{
+#endif
+
+
+
+#ifdef __cplusplus
+	}
+#endif
+//file end
---
+++ hzrqRedo/hzrq_bc95/src/app/nb_routines.h
@@ -0,0 +1,13 @@
+//file name :nb_routines.h
+#ifndef __nb_routines_h__
+#define __nb_routines_h__
+#ifdef __cplusplus
+	extern "C"{
+#endif
+
+
+
+#ifdef __cplusplus
+	}
+#endif
+//file end
